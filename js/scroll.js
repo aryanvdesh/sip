7c65cf2ca38467b6c8d0449950699f51bec3ece4
Aryan <aryanvdeshpande@gmail.com>
2020-08-29 19:36:03 +0530

    navbar

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 300) {
-          $(".navbar").css("background" , "blue");
-        }
-  
-        else{
-            $(".navbar").css("background" , "transparent");  	
-        }
-    })
-  })
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
